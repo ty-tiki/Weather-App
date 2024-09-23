cd240206c629cf6ecd4aeef4e283cf0731a3f3ba
ty-tiki <young.toni@gmail.com>
2024-09-23 10:19:19 +0100

    Removed HTML and used JS to inject dummy forecast data

--- src/index.js
+++ src/index.js
@@ -55,7 +55,30 @@ function search(event) {
   getCityData(searchSubmit.value);
 }
 
+function displayForecast() {
+  let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
+  let forecastHtml = "";
+
+  days.forEach(function (day) {
+    forecastHtml += `
+    <div class="weather-forecast-day">
+          <div class="weather-forecast-date">${day}</div>
+          <div class="weather-forecast-icon">☀️</div>
+          <div class="weather-forecast-temperatures">
+            <div class="weather-forecast-temperature">
+              <strong>15°</strong>
+            </div>
+            <div class="weather-forecast-temperature">9°</div>
+          </div>
+        </div>
+        `;
+  });
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = forecastHtml;
+}
+
 let searchForm = document.querySelector("#search-form");
 searchForm.addEventListener("submit", search);
 
 getCityData("London");
+displayForecast();
